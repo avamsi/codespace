48a595167a85144af2495dfc4eefa7a9732e59bc
Vamsi Avula <avamsi07@gmail.com>
2023-04-24 17:46:09 +0000

    automatically install fzf (and some minor changes)

--- setup.zsh
+++ setup.zsh
@@ -3,10 +3,11 @@
 # Usage: setup.zsh
 
 brew bundle --file=/workspaces/codespace/brewfile --no-lock
+"$HOMEBREW_PREFIX/opt/fzf/install" --all --no-bash --no-update-rc
 
-cargo install --git https://github.com/martinvonz/jj.git --locked --bin jj jujutsu
+cargo install --git https://github.com/martinvonz/jj --locked --bin jj jujutsu
 
-git clone --depth=1 --recurse-submodules --shallow-submodules https://github.com/avamsi/dotfiles /home/vscode/dotfiles
+git clone --depth=1 --recurse-submodules --shallow-submodules https://github.com/avamsi/dotfiles ~/dotfiles
 
 go install github.com/avamsi/dotfiles@latest
 go install github.com/avamsi/rd@latest
