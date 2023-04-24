d9cc004bc28443af5b9b03ee32331147ff2b0e37
Vamsi Avula <avamsi07@gmail.com>
2023-04-24 16:46:55 +0000

    cargo install jj and force link dotfiles

--- setup.zsh
+++ setup.zsh
@@ -2,7 +2,9 @@
 #
 # Usage: setup.zsh
 
-brew bundle --file=/workspaces/codespace/brewfile
+brew bundle --file=/workspaces/codespace/brewfile --no-lock
+
+cargo install --git https://github.com/martinvonz/jj.git --locked --bin jj jujutsu
 
 git clone --depth=1 --recurse-submodules --shallow-submodules https://github.com/avamsi/dotfiles /home/vscode/dotfiles
 
@@ -10,4 +12,4 @@ go install github.com/avamsi/dotfiles@latest
 go install github.com/avamsi/rd@latest
 go install github.com/avamsi/tmuxl@latest
 
-dotfiles link
+dotfiles link --force
