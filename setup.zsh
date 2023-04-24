1bef4505cace58bc8d1366998a64962ec27fecda
Vamsi Avula <avamsi07@gmail.com>
2023-04-24 17:48:31 +0530

    add brewfile (and move gh there)

--- setup.zsh
+++ setup.zsh
@@ -2,6 +2,12 @@
 #
 # Usage: setup.zsh
 
+(cd ~ && git clone --depth=1 --recurse-submodules --shallow-submodules https://github.com/avamsi/dotfiles)
+
+brew bundle --file=brewfile
+
 go install github.com/avamsi/dotfiles@latest
 go install github.com/avamsi/rd@latest
 go install github.com/avamsi/tmuxl@latest
+
+dotfiles link
